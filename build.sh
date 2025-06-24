ea440bd43f01fe6b7940c57be13681290ae6e6a0
WLiam-Dev <jadosone2@gmail.com>
2025-06-23 20:54:09 -0400

    casi

--- build.sh
+++ build.sh
@@ -2,14 +2,18 @@
 
 mkdir -p bin
 
-# Descarga AppImage
-curl -L -o bin/wkhtmltopdf https://github.com/wkhtmltopdf/packaging/releases/download/0.12.6-1/wkhtmltox-0.12.6-1.alpine3.17_amd64.AppImage
+# 1. Descarga el AppImage
+curl -L -o bin/wkhtmltopdf.appimage https://github.com/wkhtmltopdf/packaging/releases/download/0.12.6-1/wkhtmltox-0.12.6-1.alpine3.17_amd64.AppImage
 
-# Dale permisos de ejecución
-chmod +x bin/wkhtmltopdf
+# 2. Da permisos
+chmod +x bin/wkhtmltopdf.appimage
 
-# Monta AppImage una vez (esto la "extrae")
-bin/wkhtmltopdf --appimage-extract
+# 3. Extrae el contenido
+bin/wkhtmltopdf.appimage --appimage-extract
 
-# Usa el ejecutable real
+# 4. Mueve el ejecutable real a bin/wkhtmltopdf
 mv squashfs-root/usr/local/bin/wkhtmltopdf bin/wkhtmltopdf
+
+# 5. Limpieza (opcional)
+rm -rf squashfs-root
+rm bin/wkhtmltopdf.appimage
